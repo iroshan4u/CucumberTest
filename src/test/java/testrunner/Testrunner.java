c03ad297048644181c48508bef4afbf3385480cf
Iroshan Bandara <iroshan4u@gmail.com>
2024-06-29 11:56:55 +0200

    minor changes

--- src/test/java/testrunner/Testrunner.java
+++ src/test/java/testrunner/Testrunner.java
@@ -5,6 +5,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
+//'tags' - use the @tags in feature file
 @CucumberOptions(features="C:\\Users\\Iroshan\\eclipse-workspace\\Cucumber\\src\\test\\java\\featurespkg", 
 glue={"stepsdefinitionpkg"}, tags="@regression or @smoke")
 
